e200c7fb4a76c1b35a3563c938a8621e7d3e98d5
bimmbim <bimabaraja76@gmail.com>
2025-05-08 19:44:53 +0700

    commit10

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -93,7 +93,22 @@ public:
         return true;
     }
 
-    
+    void traverse()
+    {
+        if (listEmpty())
+            cout << "\nList Kosong\n";
+        else
+        {
+            cout << "\nData di dalam list adalah:\n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << " ";
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+    }
 };
 
 
